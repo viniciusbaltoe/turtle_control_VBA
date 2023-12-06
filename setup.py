0c629e377fb9c087d327b75c2712db4a43635bf6
Vinícius Breda Altoé <viniciusbaltoe@gmail.com>
2023-12-06 16:53:53 -0300

    Add launch file for lab5

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'turtle_control_VBA'
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include Gazebo worlds (assuming they are located in a 'worlds' directory)
+        #(os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world'))),
+        # Add Gazebo launch files if applicable
+        #(os.path.join('share', package_name, 'gazebo'), glob(os.path.join('gazebo', '*.launch.py'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
